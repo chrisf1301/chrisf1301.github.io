776a9fee18a712b4681abf1f248d3b31d848c16c
chrisf1301 <feuchter@email.sc.edu>
2025-09-09 21:58:37 -0400

    push example and fixed wireframe

---
+++ csce242/exercises/04_js/js/intro.js
@@ -0,0 +1,13 @@
+/*
+const sayHello = () => {
+    console.log("hello world");
+} 
+
+document.getElementById("btn-click-me").oneclick= sayHello;
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("hello world"); 
+}; 
+
+
